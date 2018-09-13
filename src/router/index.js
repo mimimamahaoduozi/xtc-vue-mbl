b12be7c265cba679ea493aa294ec7125039cfbfd
myf1001 <767352435@qq.com>
2018-09-13 16:32:46 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,26 @@ Vue.use(Router)
 const router = new Router({
     routes: [
         // 按需加载（推荐）
-        {
-            path: '/',
-            name: 'HomePage',
-            // component: resolve => require(['@/view/home-page/index.vue'], resolve)
-        },
+        // {
+        //     path: '/',
+        //     name: 'HomePage',
+        //     component: resolve => require(['@/view/home-page/index.vue'], resolve)
+        // },
+      {
+        path: '/',
+        name: 'HomePage',
+        component: resolve => require(['@/view/home-page/produce.vue'], resolve)
+      },
     ]
-});
+})
 
 router.beforeEach((to, from, next) => {
     window.scrollTo(0, 0)
     next()
-});
+})
 
 router.beforeResolve((to, from, next) => {
     next()
-});
+})
 
 export default router
